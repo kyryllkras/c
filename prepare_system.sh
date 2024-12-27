59dc9608911fc3b701ffbc7fa73e59228d527f4b
Kyryl Kras <kyryll.kras@gmail.com>
2024-12-27 19:30:17 +0200

    first commit

--- prepare_system.sh
+++ prepare_system.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt update -y && apt upgrade -y
-apt install grub2 wimtools ntfs-3g -y
+apt install grub2 wimtools ntfs-3g parted util-linux -y
 echo "Система оновлена та необхідне ПЗ встановлено."
