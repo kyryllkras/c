b7acdd1bcafc68b5e9b1dccf9da4630228fb03bd
Kyryl Kras <kyryll.kras@gmail.com>
2024-12-29 15:00:08 +0200

    now

--- 07-install-win.sh
+++ 07-install-win.sh
@@ -7,7 +7,7 @@ cd /root/windisk
 # Завантаження Windows 10 ISO
 iso_path="win10.iso"
 echo "Завантаження Windows 10 ISO..."
-wget -O "$iso_path" https://example.com/win10.iso || { echo "Не вдалося завантажити ISO!"; exit 1; }
+wget -O "$iso_path" https://download851.mediafire.com/bn3mqht8fpggiO7oXcvvZoq_YtQ3_HfHb_9oWZWVhrcHwoNlX2wnDSbcWRDeBjSf-PSoMCS28-8d3b2b0qWzyw7YCa3QPI6tDPOcwPtJZGjaVIUtrONcLI5H90rNcclpTmsa6dajJPOpSQlnh71UNWzccxVlv38l6o6WLg0SSAnOpw/bh54h0pgw5gz4sh/win10.iso || { echo "Не вдалося завантажити ISO!"; exit 1; }
 
 # Монтування ISO
 mkdir -p winfile
--- 08-add-virtio-drivers.sh
+++ 08-add-virtio-drivers.sh
@@ -4,7 +4,7 @@ set -e
 # Завантаження VirtIO ISO
 iso_path="virtio.iso"
 echo "Завантаження VirtIO ISO..."
-wget -O "$iso_path" https://example.com/virtio.iso || { echo "Не вдалося завантажити VirtIO ISO!"; exit 1; }
+wget -O "$iso_path" https://bit.ly/4d1g7Ht || { echo "Не вдалося завантажити VirtIO ISO!"; exit 1; }
 
 # Монтування VirtIO
 mount -o loop "$iso_path" winfile
